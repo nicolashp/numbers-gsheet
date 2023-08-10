741d65d51c1c6866e1bf0ea622bc978ead3e4d4f
nicolashp <50198898+nicolashp@users.noreply.github.com>
2023-08-10 14:32:36 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const app = express();
-const port = 3000;
 
 // Route pour la page d'accueil
 app.get('/', (req, res) => {
@@ -9,11 +8,9 @@ app.get('/', (req, res) => {
 
 // URL de redirection - vous pouvez le configurer selon vos besoins
 app.get('/redirect', (req, res) => {
-  // Traitement de la logique de redirection ici...
-  // Par exemple, récupérer des données de la requête (req.query) et faire des actions spécifiques.
   res.send('Vous avez été redirigé ici !');
 });
 
-app.listen(port, () => {
-  console.log(`App listening at http://localhost:${port}`);
-});
+// Notez que nous ne lançons pas le serveur ici. 
+// Nous exportons simplement l'application pour que Vercel puisse la gérer.
+module.exports = app;
