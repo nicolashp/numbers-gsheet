39be968e42df2ce1458824d156a830fa394d32ed
nicolashp <50198898+nicolashp@users.noreply.github.com>
2023-08-10 15:18:32 +0200

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,21 @@ app.get('/', (req, res) => {
 
 // URL de redirection
 app.get('/redirect', (req, res) => {
-  res.send('Vous avez été redirigé ici !');
+  const requisitionId = req.query.requisition_id;
+  if (requisitionId) {
+    res.send(`
+      <html>
+        <body>
+          <script>
+            // Utilisez window.postMessage pour envoyer le requisition_id à la fenêtre parente
+            window.parent.postMessage({ type: 'REQUISITION_ID', data: '${requisitionId}' }, '*');
+          </script>
+        </body>
+      </html>
+    `);
+  } else {
+    res.send('Erreur : requisition_id manquant dans l\'URL.');
+  }
 });
 
 // Exportez l'application pour Vercel
